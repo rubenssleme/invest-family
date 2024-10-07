531b0b1498aec113efe603c36b722ac880738eb0
Rubens Leme <rubenss.leme@gmail.com>
2024-10-07 10:15:36 -0300

    ajuste na cadastrar.php inclusão do header, footer e formulario

--- cadastrar.php
+++ cadastrar.php
@@ -3,6 +3,8 @@
 require __DIR__.'/vendor/autoload.php';
 
 define('TITLE','Cadastrar vaga');
+use \App\Controller\Pages\Header;
+use \App\Controller\Pages\Footer;
 
 use \App\Entity\Vaga;
 $obVaga = new Vaga;
@@ -19,6 +21,10 @@ if(isset($_POST['titulo'],$_POST['descricao'],$_POST['ativo'])){
   exit;
 }
 
-include __DIR__.'/includes/header.php';
+
+//include __DIR__.'/includes/header.php';
+  echo Header::getHeader();
+//include __DIR__.'/includes/listagem.php';
 include __DIR__.'/includes/formulario.php';
-include __DIR__.'/includes/footer.php';
+  echo Footer::getFooter();
+//include __DIR__.'/includes/footer.php';
